53e943a9a4ad7c3c58b5e4917aa751c6b1c74ebf
byrdjess <byrdjess@me.com>
2019-02-23 23:49:14 -0700

    resettwo

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -21,20 +21,18 @@ $(document).ready(function() {
     var counter = 0;
 
     var numberOptions = [Math.floor(Math.random() * 11) + 1, Math.floor(Math.random() * 11) + 1, Math.floor(Math.random() * 11) + 1, Math.floor(Math.random() * 11) + 1];
-
+  
     for (var i = 0; i < numberOptions.length; i++) {
 
         var imageJewel = $("<img>");
         imageJewel.addClass("jewel-image");
-        imageJewel.attr("src", "http://cdn.playbuzz.com/cdn/35910209-2844-45c0-b099-f4d82878d54f/00261fda-4062-4096-81fd-8cf96b9034e8.jpg")
+        imageJewel.attr("src", "http://cdn.playbuzz.com/cdn/35910209-2844-45c0-b099-f4d82878d54f/00261fda-4062-4096-81fd-8cf96b9034e8.jpg");
         imageJewel.attr("data-jewel-value", numberOptions[i]);
         $("#jewel" + (i+1)).append(imageJewel);
 
         console.log(numberOptions[i]);
 
     }
-
-   
     
 
     $(".jewel-image").on("click", function() {
@@ -47,39 +45,46 @@ $(document).ready(function() {
 
         $(".addedScore").html("Your new score is: " + counter);
 
+
         if (counter === randomNumber) {
             wins++;
-            counter = 0;
-            console.log("You win!");
-            console.log(wins);
-            $(".statement").text("You lost! Try Again.");
+            $(".statement").text("You won Can you do it again?");
             $(".winsCount").text("Wins: " + wins);
+            if (wins++) {
+            reset();
+            }
             
             
         }
 
         else if (counter >= randomNumber) {
             losses++;
-            counter = 0;
-            console.log("You lose!");
-            console.log(losses);
-           
             $(".statement").text("You lost! Try Again.");
             $(".lossesCount").text("Losses: " + losses);
-            
+            if(losses++){
+                reset();
+            }
             
         }
+
+        function reset() {
+
+            counter = 0;
+            $(".addedScore").html("Your new score is: " + counter);
+
+            randomNumber = Math.floor(Math.random() * 101) + 19;
+            $(".compuRandom").html(randomNumber);
+
+            
+        
+            }
+        
+
+
 
     
       });
 
-      function reset () {
-        
-    }
-
-
-
-      reset ();
     
 
 });
