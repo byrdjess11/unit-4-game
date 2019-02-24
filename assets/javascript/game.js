6af885f74d18971cab5656fbf392923e209e4da5
byrdjess <byrdjess@me.com>
2019-02-24 02:26:02 -0700

    css

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -26,7 +26,7 @@ $(document).ready(function() {
 
         var imageJewel = $("<img>");
         imageJewel.addClass("jewel-image");
-        imageJewel.attr("src", "http://cdn.playbuzz.com/cdn/35910209-2844-45c0-b099-f4d82878d54f/00261fda-4062-4096-81fd-8cf96b9034e8.jpg");
+        imageJewel.attr("src", "http://getdrawings.com/cliparts/sun-clipart-21.png");
         imageJewel.attr("data-jewel-value", numberOptions[i]);
         $("#jewel" + (i+1)).append(imageJewel);
 
@@ -48,10 +48,10 @@ $(document).ready(function() {
 
         if (counter === randomNumber) {
             wins++;
-            $(".statement").text("You won Can you do it again?");
+            $(".statement").text("You won! Can you do it again?");
             $(".winsCount").text("Wins: " + wins);
             if (wins++) {
-            reset();
+                reset();
             }
             
             
@@ -69,13 +69,12 @@ $(document).ready(function() {
 
         function reset() {
 
+
             counter = 0;
             $(".addedScore").html("Your new score is: " + counter);
 
             randomNumber = Math.floor(Math.random() * 101) + 19;
             $(".compuRandom").html(randomNumber);
-
-            
         
             }
         
